a51c716f0ddc4a6cd8d743e7c94e50c51cf14282
zzzccca <542495062@qq.com>
2018-06-13 16:12:18 +0800

    啊啊啊

---
+++ src/main/java/com/elk/elk/ElkApplication.java
@@ -0,0 +1,22 @@
+package com.elk.elk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkApplication implements CommandLineRunner {
+
+	private final static Logger logger= LoggerFactory.getLogger(ElkApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		logger.info("=========hello World========");
+	}
+}
